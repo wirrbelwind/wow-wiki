a6f1853a22ce96e7ea72fef68ef8b0fa00ccbcaf
igor komendantov <komendantov2580@gmail.com>
2024-09-25 23:25:01 +0300

    test server action

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -1,6 +1,11 @@
-export default async function ItemPage({ params: {id} }: { params: { id: string } }) {
-	const itemResponse = await fetch(`/api/item/${id}`)
-	const item = await itemResponse.json()
+import { getItem } from "@/app/actions"
+
+export default async function ItemPage({ params: { id } }: { params: { id: string } }) {
+	// const itemResponse = await fetch(`/api/item/${id}`)
+	// const item = await itemResponse.json()
+
+	const item = await getItem(id)
+	console.log(item)
 	return (
 		<div>
 			{JSON.stringify(item)}
