26b7aeadf2b5f294eef0f7b2e135f3341866da43
igor komendantov <komendantov2580@gmail.com>
2024-09-28 19:05:33 +0300

    auth: create a login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,16 @@
+import { Button, Input } from "@nextui-org/react";
+
 export default function LoginPage() {
 	return (
-		<div>
-			Login page
+		<div className="h-dvh">
+			<div className="flex items-center justify-center h-full">
+				<form className="w-5/12">
+					<Input type="text" label="Client ID" />
+					<Input type="text" label="Client secret" className="mt-4"/>
+
+					<Button className="w-full mt-8">Submit</Button>
+				</form>
+			</div>
 		</div>
 	)
 }
