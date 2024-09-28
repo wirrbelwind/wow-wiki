db50994d1cc89a08eda8857e76ea81ed402e95dc
igor komendantov <komendantov2580@gmail.com>
2024-09-28 18:23:33 +0300

    next-ui: use nextui plugin in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
@@ -14,6 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
