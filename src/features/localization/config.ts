a08c92649a5125acc02edcae1a8ec78aa1d9f552
igor komendantov <komendantov2580@gmail.com>
2024-09-29 22:22:34 +0300

    lang: create array of locales

---
+++ src/features/localization/config.ts
@@ -0,0 +1,16 @@
+import { Locale } from "../type";
+
+export const localeList: Locale[] = [
+	{ value: "en-us", title: "English (United States)" },
+	{ value: "es-mx", title: "Español (México)" },
+	{ value: "pt-br", title: "Português (Brasil)" },
+	{ value: "de-de", title: "Deutsch (Deutschland)" },
+	{ value: "en-gb", title: "English (Great Britain)" },
+	{ value: "es-es", title: "Español (España)" },
+	{ value: "fr-fr", title: "Français (France)" },
+	{ value: "it-it", title: "Italiano (Italia)" },
+	{ value: "ru-ru", title: "Русский (Россия)" },
+	{ value: "ko-kr", title: "한국어 (대한민국)" },
+	{ value: "zh-tw", title: "中文 (繁體)" },
+	{ value: "zh-cn", title: "中文 (简体)" },
+]
