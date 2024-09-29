2ae82ae462e101eb94dc60daa6733ba20199f7ff
igor komendantov <komendantov2580@gmail.com>
2024-09-29 22:21:55 +0300

    lang: delete outdated changeLanguage server action

--- src/features/localization/changeLanguage.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers"
-
-export const changeLanguage = (form: FormData) => {
-	const lang = form.get("lang")
-
-	const cookiesManager = cookies()
-	cookiesManager.set("lang", lang)
-}
