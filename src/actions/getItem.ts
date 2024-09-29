4dd898cbe939249bfa5b55bf36ab88325f549ef4
igor komendantov <komendantov2580@gmail.com>
2024-09-29 12:57:30 +0300

    item:temp: change default locale

--- src/actions/getItem.ts
+++ src/actions/getItem.ts
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 export async function getItem(id: number) {
 	const accessToken = cookies().get(ACCESS_TOKEN_COOKIES_KEY)?.value
 
-	const itemResponse = await fetch(`https://eu.api.blizzard.com/data/wow/item/${id}?namespace=static-eu&locale=en_US&access_token=${accessToken}`)
+	const itemResponse = await fetch(`https://eu.api.blizzard.com/data/wow/item/${id}?namespace=static-eu&locale=ru_RU&access_token=${accessToken}`)
 
 	const item = await itemResponse.json()
 	return item
