dc3c3573f2e4bd627ba156120d9e2d6840240eeb
igor komendantov <komendantov2580@gmail.com>
2024-10-05 15:04:41 +0300

    lang: change import name

--- src/features/localization/index.ts
+++ src/features/localization/index.ts
@@ -1,3 +1,4 @@
+export { getActiveLocale } from './model/selectedLanguage'
 export { languageTag } from './model/paraglide/runtime'
 export { paraglideMiddleware } from './model/i18n'
 export { LanguageSelector } from './ui/LanguageSelector'
