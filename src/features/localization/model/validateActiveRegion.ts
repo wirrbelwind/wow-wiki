5f16f4e316bfa59af7f4243569da9eb57e61bbbd
igor komendantov <komendantov2580@gmail.com>
2024-10-05 23:22:54 +0300

    lang: rewrite validateActiveRegion

--- src/features/localization/model/validateActiveRegion.ts
+++ src/features/localization/model/validateActiveRegion.ts
@@ -1,26 +1,18 @@
 
-'use server'
-
 import { cookies } from "next/headers"
 import { BN_REGION_COOKIE_KEY, defaultRegionKeyBN, regionsOptions } from "../config"
 import { RegionKeyBN } from "../types"
 import { getActiveLocale } from "./getActiveLocale"
 
 export const validateActiveRegion = () => {
-	if (!defaultRegionKeyBN) {
-		throw new Error(`Key of default region is: ${defaultRegionKeyBN}`)
-	}
+	// if (!defaultRegionKeyBN) {
+	// 	throw new Error(`Key of default region is: ${defaultRegionKeyBN}`)
+	// }
 
-	const defaultRegionData = regionsOptions.find(region => region.value === defaultRegionKeyBN)
+	// const defaultRegionData = regionsOptions.find(region => region.value === defaultRegionKeyBN)
 
-	if (!defaultRegionData) {
-		throw new Error(`Data of default region ${defaultRegionKeyBN} not found`)
-	}
-
-	// const defaultRegion = {
-	// 	regionKey: defaultRegionKeyBN,
-	// 	hosting: regionHostingList[defaultRegionKeyBN],
-	// 	localeKey: defaultRegionData.availableLocales[0]
+	// if (!defaultRegionData) {
+	// 	throw new Error(`Data of default region ${defaultRegionKeyBN} not found`)
 	// }
 
 	const cookiesManager = cookies()
@@ -28,23 +20,25 @@ export const validateActiveRegion = () => {
 	const activeRegionKey = cookiesManager.get(BN_REGION_COOKIE_KEY)?.value as RegionKeyBN
 	const activeLocale = getActiveLocale()
 
-	if (!activeRegionKey) {
-		cookiesManager.set(BN_REGION_COOKIE_KEY, defaultRegionKeyBN)
-		cookiesManager.set('NEXT_LOCALE', defaultRegionData.availableLocales[0])
+	if (!activeRegionKey || !activeLocale) {
+		// cookiesManager.set(BN_REGION_COOKIE_KEY, defaultRegionKeyBN)
+		// cookiesManager.set('NEXT_LOCALE', defaultRegionData.availableLocales[0])
+		return false
 	}
 
-	if (activeRegionKey) {
-		const activeRegionData = regionsOptions.find(region => region.value === activeRegionKey)
+	const activeRegionData = regionsOptions.find(region => region.value === activeRegionKey)
 
-		if (!activeRegionData) {
-			throw new Error(`Data of region ${activeRegionKey} not found`)
-		}
-
-		const isActiveLocaleValid = activeRegionData.availableLocales.includes(activeLocale)
-
-		if (!activeLocale || !isActiveLocaleValid) {
-			cookiesManager.set('NEXT_LOCALE', activeRegionData?.availableLocales[0])
-		}
-
+	if (!activeRegionData) {
+		// throw new Error(`Data of region ${activeRegionKey} not found`)
+		return false
 	}
+
+	const isActiveLocaleValid = activeRegionData.availableLocales.includes(activeLocale)
+
+	if (!isActiveLocaleValid) {
+		// cookiesManager.set('NEXT_LOCALE', activeRegionData?.availableLocales[0])
+		return false
+	}
+
+	return true
 }
