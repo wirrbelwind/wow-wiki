ad9f7692ce3f00b8ca671b6a0a633874196f9438
igor komendantov <komendantov2580@gmail.com>
2024-10-05 15:02:04 +0300

    lang: create config for loalec and regions

--- src/features/localization/config.ts
+++ src/features/localization/config.ts
@@ -1,6 +1,6 @@
-import { Locale } from "./types";
+import { RegionBattleNetHost, RegionBattleNetOption, RegionBattleNetTag, SelectLocaleOption } from "./types";
 
-export const localeList: Locale[] = [
+export const localeOptions: SelectLocaleOption[] = [
 	{ value: "en-us", title: "English (United States)" },
 	{ value: "es-mx", title: "Español (México)" },
 	{ value: "pt-br", title: "Português (Brasil)" },
@@ -13,4 +13,44 @@ export const localeList: Locale[] = [
 	{ value: "ko-kr", title: "한국어 (대한민국)" },
 	{ value: "zh-tw", title: "中文 (繁體)" },
 	{ value: "zh-cn", title: "中文 (简体)" },
+]
+
+export const regionHostingList: Record<RegionBattleNetTag, RegionBattleNetHost> = {
+	'us': 'https://us.api.blizzard.com',
+	'eu': 'https://eu.api.blizzard.com',
+	'kr': 'https://kr.api.blizzard.com',
+	'tw': 'https://tw.api.blizzard.com',
+	'cn': 'https://gateway.battlenet.com.cn'
+}
+
+export const regionOptions: RegionBattleNetOption[] = [
+	{
+		value: 'us',
+		title: 'North America',
+		availableLocales: [
+			'en_US', 'es_MX', 'pt_BR'
+		]
+	},
+	{
+		value: 'eu',
+		title: 'Europe',
+		availableLocales: [
+			'en_GB',
+			'es_ES',
+			'fr_FR',
+			'ru_RU',
+			'de_DE',
+			'pt_BR',
+			'it_IT'
+		]
+	},
+	{
+		value: 'kr',
+		title: 'Korea',
+		availableLocales: [
+			'ko_KR',
+			'zh_TW',
+			'zh_CN'
+		]
+	}
 ]
