2cd553577a3cb61e86080f8f8803f57c7389a872
igor komendantov <komendantov2580@gmail.com>
2024-10-05 15:18:01 +0300

    lang: rename helper

--- src/features/localization/model/bnLocaleToParaglideLocale.ts
+++ src/features/localization/model/bnLocaleToParaglideLocale.ts
@@ -1,6 +1,6 @@
 import { LocaleBN, ParaglideLocale } from "../types";
 
-export const getBNLocale = (locale: ParaglideLocale): LocaleBN => {
+export const bnLocaleToParaglideLocale = (locale: ParaglideLocale): LocaleBN => {
 	const [language, region] = locale.split('-')
 
 	return `${language}_${region.toUpperCase()}` as LocaleBN
