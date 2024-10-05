24a6b407bc7237db9a541595fad135529dd72345
igor komendantov <komendantov2580@gmail.com>
2024-10-05 15:05:47 +0300

    header: render region selector

--- src/widgets/GlobalHeader/ui/GlobalHeader.tsx
+++ src/widgets/GlobalHeader/ui/GlobalHeader.tsx
@@ -1,10 +1,12 @@
 import { LanguageSelector } from "@/features/localization"
+import { RegionSelector } from "@/features/localization/ui/RegionSelector"
 
 export const GlobalHeader = () => {
 	return (
 		<div className="border-b-orange-600 border-b-2 flex px-4 py-6">
 			<div className="ml-auto">
 				<LanguageSelector />
+				<RegionSelector />
 			</div>
 		</div>
 	)
