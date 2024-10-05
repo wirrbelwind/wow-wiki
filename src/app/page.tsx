ac8ab8fdc60c4b7358636026ea1d0b3815b40759
igor komendantov <komendantov2580@gmail.com>
2024-10-05 15:06:04 +0300

    lang: temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { getActiveLocale } from "@/features/localization/model/getActiveLocale";
+import { cookies } from "next/headers";
 
 export default async function Home() {
 
   return (
     <div>
-      home
+      <div>
+        LANG: {getActiveLocale()}
+      </div>
+
+      <div>
+        REGION: {cookies().get('BN_REGION')?.value}
+      </div>
     </div>
   );
 }
