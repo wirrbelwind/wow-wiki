4ffa957c615b248e677f9dea3d57bbface292092
igor komendantov <komendantov2580@gmail.com>
2024-10-05 15:01:24 +0300

    create types for locales and regions

--- src/features/localization/types.ts
+++ src/features/localization/types.ts
@@ -1,6 +1,23 @@
 import { AvailableLanguageTag } from "./model/paraglide/runtime"
 
-export interface Locale {
+export type ParaglideLocale = AvailableLanguageTag
+export type LocaleBattleNet = "en_US" | "es_MX" | "pt_BR" | "de_DE" | "en_GB" | "es_ES" | "fr_FR" | "it_IT" | "ru_RU" | "ko_KR" | "zh_TW" | "zh_CN"
+
+export interface SelectLocaleOption {
 	title: string
-	value: AvailableLanguageTag
+	value: ParaglideLocale
 }
+
+export type RegionBattleNetTag = 'cn' |
+	'tw' |
+	'kr' |
+	'eu' |
+	'us'
+
+export interface RegionBattleNetOption {
+	value: RegionBattleNetTag
+	title: string
+	availableLocales: LocaleBattleNet[]
+}
+
+export type RegionBattleNetHost = string
