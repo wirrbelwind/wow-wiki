4977fb7e7c69ba3e0e9746f953032c56c79a3242
igor komendantov <komendantov2580@gmail.com>
2024-10-06 17:40:47 +0300

    item: use axios in request

--- src/actions/getItem.ts
+++ src/actions/getItem.ts
@@ -2,7 +2,7 @@
 
 import { getUserCredentials } from "@/features/auth/model/getUserCredentials";
 import { getLocation } from "@/features/localization";
-import { localeToBNLocale } from "@/features/localization/model/convert-locale/localeToBNLocale";
+import axios from "axios";
 
 export async function getItem(id: number) {
 	const {
@@ -11,15 +11,17 @@ export async function getItem(id: number) {
 
 	const { localeBN, region, regionHosting } = getLocation()
 
-	try {
-		// console.log(`${hosting}/data/wow/item/47549?namespace=static-${regionKey}&locale=${locale}&access_token=${accessToken}`)
-		console.log(`${regionHosting}/data/wow/item/${id}?namespace=static-${region}&locale=${localeBN}&access_token=${accessToken}`)
-		const itemResponse = await fetch(`${regionHosting}/data/wow/item/${id}?namespace=static-${region}&locale=${localeBN}&access_token=${accessToken}`)
+	const response = await axios.get(`${regionHosting}/data/wow/item/${id}`, {
+		params: {
+			namespace: `static-${region}`,
+			locale: localeBN,
+			access_token: accessToken
+		}
+	})
 
-		const item = await itemResponse.json()
-		return item
-	}
-	catch (e) {
-		return e
-	}
+	response.status
+	// const response = await fetch(`${regionHosting}/data/wow/item/${id}?namespace=static-${region}&locale=${localeBN}&access_token=${accessToken}`)
+
+	const item = await response.data
+	return item
 }
