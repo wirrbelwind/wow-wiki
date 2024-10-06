0524ad443e5358eedbeadb69e1a6aea4b6a24dd3
igor komendantov <komendantov2580@gmail.com>
2024-10-06 16:10:24 +0300

    lang: rewrite helper validateActiveRegion

--- src/features/localization/model/validateActiveRegion.ts
+++ src/features/localization/model/validateActiveRegion.ts
@@ -1,44 +1,42 @@
 
 import { cookies } from "next/headers"
-import { BN_REGION_COOKIE_KEY, defaultRegionKeyBN, regionsOptions } from "../config"
+import { BN_REGION_COOKIE_KEY, regionsOptions } from "../config"
 import { RegionKeyBN } from "../types"
 import { getActiveLocale } from "./getActiveLocale"
 
-export const validateActiveRegion = () => {
-	// if (!defaultRegionKeyBN) {
-	// 	throw new Error(`Key of default region is: ${defaultRegionKeyBN}`)
-	// }
+export interface RegionValidationResult {
+	type: 'fulfilled' | 'conflict' | 'empty'
+}
 
-	// const defaultRegionData = regionsOptions.find(region => region.value === defaultRegionKeyBN)
-
-	// if (!defaultRegionData) {
-	// 	throw new Error(`Data of default region ${defaultRegionKeyBN} not found`)
-	// }
-
+export const validateActiveRegion = (): RegionValidationResult => {
 	const cookiesManager = cookies()
 
 	const activeRegionKey = cookiesManager.get(BN_REGION_COOKIE_KEY)?.value as RegionKeyBN
 	const activeLocale = getActiveLocale()
 
 	if (!activeRegionKey || !activeLocale) {
-		// cookiesManager.set(BN_REGION_COOKIE_KEY, defaultRegionKeyBN)
-		// cookiesManager.set('NEXT_LOCALE', defaultRegionData.availableLocales[0])
-		return false
+		return {
+			type: 'empty'
+		}
 	}
 
 	const activeRegionData = regionsOptions.find(region => region.value === activeRegionKey)
 
 	if (!activeRegionData) {
-		// throw new Error(`Data of region ${activeRegionKey} not found`)
-		return false
+		return {
+			type: 'conflict'
+		}
 	}
 
 	const isActiveLocaleValid = activeRegionData.availableLocales.includes(activeLocale)
 
 	if (!isActiveLocaleValid) {
-		// cookiesManager.set('NEXT_LOCALE', activeRegionData?.availableLocales[0])
-		return false
+		return {
+			type: 'conflict'
+		}
 	}
 
-	return true
+	return {
+		type: 'fulfilled'
+	}
 }
