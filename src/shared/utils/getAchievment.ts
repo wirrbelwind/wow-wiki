f5a1deb5827222bfb265a40b3ab71eec59381753
igor komendantov <komendantov2580@gmail.com>
2024-10-13 15:05:14 +0300

    api: create getAchievement util

--- src/shared/utils/getAchievment.ts
+++ src/shared/utils/getAchievment.ts
@@ -1,58 +1,17 @@
 import axios from "axios"
 import { getUser } from "./getUser"
+import { AchievementResponse } from "../types"
+import { getMedia } from "./getMedia"
+import { ENTITY_TAGS } from "../constants"
 
-export interface AchievementResponse {
-	"_links": {
-		"self": {
-			"href": string
-		}
-	},
-	"id": number,
-	"category": {
-		"key": {
-			"href": string
-		},
-		"name": string,
-		"id": number
-	},
-	"name": string,
-	"description": string,
-	"points": number,
-	"is_account_wide": boolean,
-	"criteria": {
-		"id": number,
-		"description": string,
-		"amount": number
-	},
-	"next_achievement": {
-		"key": {
-			"href": string
-		},
-		"name": string,
-		"id": number
-	},
-	"media": {
-		"key": {
-			"href": string
-		},
-		"id": number
-	},
-	"display_order": number
-}
-
-export interface Achievement {
-	data: AchievementResponse
-	media: object
-}
-
-export const getAchievement = async (id: number): Promise<Achievement> => {
+export const getAchievement = async (id: string) => {
 	const {
 		credentials: { accessToken }
 	} = getUser()
 
 	const { location: { localeBN, region, regionHosting } } = getUser()
 
-	const achievementResponse = await axios.get<AchievementResponse>(`${regionHosting}/data/wow/achievement/${id}`, {
+	const achievementRequest = axios.get<AchievementResponse>(`${regionHosting}/data/wow/achievement/${id}`, {
 		params: {
 			namespace: `static-${region}`,
 			locale: localeBN,
@@ -60,16 +19,13 @@ export const getAchievement = async (id: number): Promise<Achievement> => {
 		}
 	})
 
-	const mediaResponse = await axios.get<object>(`${regionHosting}/data/wow/media/achievement/${id}`, {
-		params: {
-			namespace: `static-${region}`,
-			locale: localeBN,
-			access_token: accessToken
-		}
-	})
+	const mediaRequest = getMedia(ENTITY_TAGS.ACHIEVEMENT, parseInt(id))
+
+	const [achievementResponse, mediaResponse] = await Promise.all([achievementRequest, mediaRequest])
 
 	return {
-		data: achievementResponse.data,
-		media: mediaResponse.data,
+		...achievementResponse,
+		// Achievement API returns only 1 image
+		imageHref: mediaResponse[0].value
 	}
 }
