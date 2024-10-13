4f90ccdfd7cd2efb976400a52b460fe3fc11a7cd
igor komendantov <komendantov2580@gmail.com>
2024-10-13 17:28:15 +0300

    lang: use icon on button

--- src/features/localization/ui/ButtonOpenLocaleSelector.tsx
+++ src/features/localization/ui/ButtonOpenLocaleSelector.tsx
@@ -1,12 +1,20 @@
 'use client'
 import { Button, useDisclosure } from "@nextui-org/react";
 import { LocaleSelectorModal } from "./LocaleSelectorModal";
+import { GrLanguage } from "react-icons/gr";
 
 export const ButtonOpenLocaleSelector = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+
 	return (
 		<>
-			<Button onPress={onOpen}>Language</Button>
+			<Button
+				onPress={onOpen}
+				isIconOnly
+				className="bg-none"
+			>
+				<GrLanguage size="2rem"/>
+			</Button>
 			{isOpen && <LocaleSelectorModal closable={true} onClose={onClose} />}
 		</>
 	)
